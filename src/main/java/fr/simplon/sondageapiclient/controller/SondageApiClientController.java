baec475da76a28730ee296bf196b9782ad8574d3
Zina BREXEL <121940282+ZinaBrexel@users.noreply.github.com>
2023-04-21 13:06:09 +0200

    Ajout de l'entité et du controller Ajout des pages html Ajout du css

--- src/main/java/fr/simplon/sondageapiclient/controller/SondageApiClientController.java
+++ src/main/java/fr/simplon/sondageapiclient/controller/SondageApiClientController.java
@@ -1,2 +1,92 @@
-package fr.simplon.sondageapiclient.controller;public class SondageApiClientController {
+package fr.simplon.sondageapiclient.controller;
+
+import fr.simplon.sondageapiclient.entity.Sondage;
+import org.springframework.core.ParameterizedTypeReference;
+import org.springframework.http.*;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.client.RestTemplate;
+
+import java.util.Collection;
+
+
+@Controller
+public class SondageApiClientController {
+
+    private RestTemplate restTemplate;
+
+    public SondageApiClientController(){
+        this.restTemplate = new RestTemplate();
+    }
+
+    @GetMapping("/")
+    public String index(Model model){
+
+        String url = "http://localhost:8080/sondages";
+        ResponseEntity<Collection<Sondage>> response = restTemplate.exchange(url, HttpMethod.GET, null, new ParameterizedTypeReference<Collection<Sondage>>() {});
+        Collection<Sondage> sondages = response.getBody();
+
+        model.addAttribute("sondages", sondages);
+        return "index";
+    }
+    @GetMapping("/sondages/formulaire/new")
+    public String formSondage(Model model)
+    {
+        Sondage sondage = new Sondage();
+        model.addAttribute("Sondage", sondage);
+        model.addAttribute("titre", "Créer un nouveau sondage");
+        model.addAttribute("soustitre", "Remplir");
+
+        return "formulaire";
+    }
+
+    @GetMapping("/sondages/edition/{id}")
+    public String updateSondage(Model model, @PathVariable long id){
+
+        String url = "http://localhost:8080/sondages/{id}";
+        ResponseEntity<Sondage> response = restTemplate.getForEntity(url, Sondage.class, id);
+        Sondage sondage = response.getBody();
+
+        model.addAttribute("sondage", sondage);
+        model.addAttribute("titre", "Edition du sondage");
+
+        return "edition";
+    }
+    @GetMapping("/sondages/adios/{id}")
+    public String delEleve(Model model, @PathVariable long id)
+    {
+        String url = "http://localhost:8080/sondages/{id}";
+        restTemplate.delete(url, id);
+
+        return "redirect:/";
+    }
+
+    @PostMapping("/sondages/formulaire/new")
+    public String addSondage(@ModelAttribute("sondage") Sondage sondage){
+
+        String url = "http://localhost:8080/sondages/formulaire";
+        HttpHeaders headers = new HttpHeaders();
+        headers.setContentType(MediaType.APPLICATION_JSON);
+        HttpEntity<Sondage> request = new HttpEntity<>(sondage, headers);
+        ResponseEntity<Sondage> response = restTemplate.postForEntity(url, request, Sondage.class);
+
+        return "redirect:/";
+    }
+
+    @PostMapping("/sondages/edition/{id}")
+    public String updateSondage(@ModelAttribute("sondage") Sondage sondage, @PathVariable long id){
+
+        String url = "http://localhost:8080/sondages/formulaire/{id}";
+        HttpHeaders headers = new HttpHeaders();
+        headers.setContentType(MediaType.APPLICATION_JSON);
+        HttpEntity<Sondage> request = new HttpEntity<>(sondage, headers);
+        ResponseEntity<Sondage> response = restTemplate.exchange(url, HttpMethod.PUT, request, Sondage.class, id);
+
+        return "redirect:/";
+    }
+
 }
--- src/main/java/fr/simplon/sondageapiclient/entity/Sondage.java
+++ src/main/java/fr/simplon/sondageapiclient/entity/Sondage.java
@@ -1,2 +1,145 @@
-package fr.simplon.sondageapiclient.entity;public class Sondage {
+package fr.simplon.sondageapiclient.entity;
+
+import java.time.LocalDate;
+
+/**
+ * Entité représentant un sondage.
+ */
+
+public class Sondage {
+
+    /**
+     * Identifiant unique du sondage.
+     */
+
+    private Long id;
+
+
+    /**
+     * Description du sondage.
+     */
+
+    private String description;
+
+    /**
+     * Question posée dans le sondage.
+     */
+    private String question;
+
+    /**
+     * Date de création du sondage.
+     */
+
+    private LocalDate date_creation = LocalDate.now();
+
+
+    /**
+     * Date de clôture du sondage.
+     */
+
+    private LocalDate date_cloture;
+
+    /**
+     * Nom du créateur du sondage.
+     */
+
+    private String createur;
+
+
+
+    /**
+     * Retourne l'identifiant unique du sondage.
+     * @return l'identifiant unique du sondage.
+     */
+    public Long getId() {
+        return id;
+    }
+
+    /**
+     * Modifie l'identifiant unique du sondage.
+     * @param id Le nouvel identifiant unique du sondage.
+     */
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    /**
+     * Retourne la description du sondage.
+     * @return La description du sondage.
+     */
+    public String getDescription() {
+        return description;
+    }
+
+    /**
+     * Modifie la description du sondage.
+     * @param description La nouvelle description du sondage.
+     */
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    /**
+     * Retourne la question posée dans le sondage.
+     * @return La question posée dans le sondage.
+     */
+    public String getQuestion() {
+        return question;
+    }
+
+    /**
+     * Modifie la question posée dans le sondage.
+     * @param question La nouvelle question posée dans le sondage
+     */
+    public void setQuestion(String question) {
+        this.question = question;
+    }
+
+    /**
+     * Retourne la date de création du sondage.
+     * @return La date de création du sondage.
+     */
+    public LocalDate getDate_creation() {
+        return date_creation;
+    }
+
+    /**
+     * Modifie la date de création du sondage.
+     * @param date_creation La date de création du sondage
+     */
+    public void setDate_creation(LocalDate date_creation) {
+        this.date_creation = date_creation;
+    }
+
+    /**
+     * Retourne la date de cloture du sondage.
+     * @return La date de cloture du sondage.
+     */
+    public LocalDate getDate_cloture() {
+        return date_cloture;
+    }
+
+    /**
+     * Modifie la date de cloture du sondage.
+     * @param date_cloture La date de cloture du sondage
+     */
+    public void setDate_cloture(LocalDate date_cloture) {
+        this.date_cloture = date_cloture;
+    }
+
+    /**
+     * Retourne le créateur du sondage.
+     * @return Le créateur du sondage.
+     */
+    public String getCreateur() {
+        return createur;
+    }
+
+    /**
+     * Modifie le créateur du sondage.
+     * @param  createur Le createur du sondage.
+     */
+    public void setCreateur(String createur) {
+        this.createur = createur;
+    }
 }
